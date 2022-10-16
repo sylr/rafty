427b4c8d858a7f7f67707fb0872d09a151d96dce
Sylvain Rabot <sylvain@abstraction.fr>
2022-10-16 18:26:57 +0200

    Finish dns discoverer implementation
    Signed-off-by: Sylvain Rabot <sylvain@abstraction.fr>

--- discovery/dns/go.mod
+++ discovery/dns/go.mod
@@ -5,12 +5,13 @@ go 1.19
 require (
 	github.com/hashicorp/raft v1.3.11
 	github.com/miekg/dns v1.1.50
-	golang.org/x/net v0.0.0-20221014081412-f15817d10f9b
 	sylr.dev/rafty v0.0.0
 )
 
 require (
 	github.com/armon/go-metrics v0.4.1 // indirect
+	github.com/buraksezer/consistent v0.9.0 // indirect
+	github.com/cespare/xxhash v1.1.0 // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/hashicorp/go-hclog v1.3.1 // indirect
 	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
@@ -19,6 +20,7 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
+	golang.org/x/net v0.0.0-20221014081412-f15817d10f9b // indirect
 	golang.org/x/sys v0.0.0-20221013171732-95e765b1cc43 // indirect
 	golang.org/x/tools v0.1.12 // indirect
 )
--- discovery/dns/srv.go
+++ discovery/dns/srv.go
@@ -1,90 +1,146 @@
 package discodns
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/hashicorp/raft"
 	"github.com/miekg/dns"
-	"golang.org/x/net/context"
 
+	"sylr.dev/rafty"
 	"sylr.dev/rafty/discovery"
 )
 
-var _ discovery.Discoverer = (*DNSSRVDiscoverer)(nil)
+var _ discovery.Discoverer = (*SRVDiscoverer)(nil)
 
-type DNSSRVDiscoverer struct {
-	client     *dns.Client
-	nameserver string
-	record     string
-	interval   time.Duration
-	ch         chan []raft.Server
-	maxVoters  int
+type SRVDiscoverer struct {
+	logger         rafty.Logger
+	client         *dns.Client
+	nameserver     string
+	record         string
+	interval       time.Duration
+	ch             chan struct{}
+	maxVoters      int
+	currentServers []raft.Server
+	mux            sync.Mutex
 }
 
-func NewDNSSRVDiscoverer(client *dns.Client, nameserver string, interval time.Duration, SRVRecord string, ch chan []raft.Server) (*DNSSRVDiscoverer, error) {
-	d := &DNSSRVDiscoverer{
-		maxVoters: 10,
+type Option func(*SRVDiscoverer) error
+
+func Logger(logger rafty.Logger) Option {
+	return func(disco *SRVDiscoverer) error {
+		disco.logger = logger
+		return nil
+	}
+}
+
+func Client(client *dns.Client) Option {
+	return func(disco *SRVDiscoverer) error {
+		disco.client = client
+		return nil
+	}
+}
+
+func Interval(interval time.Duration) Option {
+	return func(disco *SRVDiscoverer) error {
+		disco.interval = interval
+		return nil
+	}
+}
+
+func Nameserver(nameserver string) Option {
+	return func(disco *SRVDiscoverer) error {
+		disco.nameserver = nameserver
+		return nil
+	}
+}
+
+func NewSRVDiscoverer(srvRecord string, options ...Option) (*SRVDiscoverer, error) {
+	d := &SRVDiscoverer{
+		maxVoters: 9,
+		interval:  10 * time.Second,
+		record:    srvRecord,
+		ch:        make(chan struct{}, 1),
 	}
 
-	if client == nil {
+	for _, option := range options {
+		if err := option(d); err != nil {
+			return nil, err
+		}
+	}
+
+	if d.client == nil {
 		d.client = &dns.Client{}
-	} else {
-		d.client = client
 	}
 
-	if interval == 0 {
-		d.interval = time.Second * 5
-	} else {
-		d.interval = interval
-	}
-
-	if len(nameserver) == 0 {
+	if len(d.nameserver) == 0 {
 		config, err := dns.ClientConfigFromFile("/etc/resolv.conf")
 		if err != nil {
 			return nil, err
 		}
 		d.nameserver = config.Servers[0]
-	} else {
-		d.nameserver = nameserver
 	}
 
-	if len(SRVRecord) == 0 {
-		return nil, errors.New("DNSSRVDiscoverer: SRVRecord is empty")
+	if len(d.record) == 0 {
+		return nil, errors.New("SRVDiscoverer: srvRecord is empty")
 	}
-
-	d.record = SRVRecord
-	d.ch = ch
 
 	return d, nil
 }
 
-func (d *DNSSRVDiscoverer) Start(ctx context.Context) (chan []raft.Server, error) {
+func (d *SRVDiscoverer) Start(ctx context.Context) error {
 	go d.run(ctx)
 
-	return d.ch, nil
+	// Immediatly signal that there are new servers
+	d.ch <- struct{}{}
+
+	return nil
 }
 
-func (d *DNSSRVDiscoverer) run(ctx context.Context) {
+func (d *SRVDiscoverer) run(ctx context.Context) {
 	for {
 		select {
 		case <-ctx.Done():
 			close(d.ch)
 			return
 		case <-time.After(d.interval):
-			d.query()
+			newServers := d.getServers()
+			if discovery.ServersDiffer(d.currentServers, newServers) {
+				d.mux.Lock()
+				d.currentServers = newServers
+				d.mux.Unlock()
+				d.ch <- struct{}{}
+			}
 		}
 	}
 }
 
-func (d *DNSSRVDiscoverer) query() {
+func (d *SRVDiscoverer) NewServers() chan struct{} {
+	return d.ch
+}
+
+func (d *SRVDiscoverer) GetServers() []raft.Server {
+	d.mux.Lock()
+	defer d.mux.Unlock()
+
+	if d.currentServers == nil {
+		d.currentServers = d.getServers()
+	}
+
+	return d.currentServers
+}
+
+func (d *SRVDiscoverer) getServers() []raft.Server {
 	msg := dns.Msg{}
 	msg.SetQuestion(d.record, dns.TypeSRV)
 	msg.SetEdns0(4096, true)
 	r, _, err := d.client.Exchange(&msg, d.nameserver)
 	if err != nil {
-		return
+		d.logger.Errorf("dns-disco: fail to query nameserver: %w", err)
+		return nil
 	}
 
 	servers := make([]raft.Server, 0, len(r.Answer))
@@ -96,13 +152,14 @@ func (d *DNSSRVDiscoverer) query() {
 				suffrage = raft.Voter
 			}
 
+			addrPort := fmt.Sprintf("%s:%d", srv.Target, srv.Port)
 			servers = append(servers, raft.Server{
-				ID:       raft.ServerID(srv.Target),
+				ID:       raft.ServerID(addrPort),
+				Address:  raft.ServerAddress(addrPort),
 				Suffrage: suffrage,
-				Address:  raft.ServerAddress(fmt.Sprintf("%s:%d", srv.Target, srv.Port)),
 			})
 		}
 	}
 
-	d.ch <- servers
+	return servers
 }
