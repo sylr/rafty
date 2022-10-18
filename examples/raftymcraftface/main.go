6179dd73239d9372536cc001dd4a9fad5fba116b
Sylvain Rabot <sylvain@abstraction.fr>
2022-10-18 10:09:19 +0200

    Tune raftymcraftface loggers
    Signed-off-by: Sylvain Rabot <sylvain@abstraction.fr>

--- examples/raftymcraftface/main.go
+++ examples/raftymcraftface/main.go
@@ -34,6 +34,8 @@ var (
 	optionNatsContext       string
 	optionNatsURL           string
 	optionLogCaller         bool
+	optionLogRaft           bool
+	optionLogConsul         bool
 	optionVerbose           int
 )
 
@@ -54,6 +56,8 @@ func init() {
 	raftyMcRaftFace.PersistentFlags().StringVar(&optionNatsURL, "nats-url", "", "Nats URL")
 	raftyMcRaftFace.PersistentFlags().CountVarP(&optionVerbose, "verbose", "v", "Increase verbosity")
 	raftyMcRaftFace.PersistentFlags().BoolVar(&optionLogCaller, "log-caller", false, "Log caller")
+	raftyMcRaftFace.PersistentFlags().BoolVar(&optionLogRaft, "log-raft", false, "Enable logs from raft library")
+	raftyMcRaftFace.PersistentFlags().BoolVar(&optionLogConsul, "log-consul", false, "Enable logs from consul library")
 }
 
 func main() {
@@ -65,58 +69,39 @@ func main() {
 }
 
 func run(cmd *cobra.Command, args []string) error {
-	zerolog.TimeFieldFormat = time.RFC3339Nano
-	consoleWriter := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: "Jan 2 15:04:05.000-0700"}
-	multi := zerolog.MultiLevelWriter(consoleWriter)
-	logger := zerolog.New(multi).With().Timestamp().Logger()
+	logger, raftylogger, raftlogger, consullogger := loggers()
 
-	switch optionVerbose {
-	case 0:
-		logger = logger.With().Logger().Level(zerolog.InfoLevel)
-	case 1:
-		logger = logger.With().Logger().Level(zerolog.DebugLevel)
-	default:
-		logger = logger.With().Logger().Level(zerolog.TraceLevel)
-	}
-
-	subLogger := logger
-	if optionLogCaller {
-		logger = logger.With().Caller().Logger()
-		subLogger = subLogger.With().CallerWithSkipFrameCount(3).Logger()
-	}
-
-	buildInfo, ok := debug.ReadBuildInfo()
-	if ok {
+	if buildInfo, ok := debug.ReadBuildInfo(); ok {
 		logger.Info().Msgf("Starting RaftyMcRaftFace version=%s go=%s", buildInfo.Main.Version, runtime.Version())
 	} else {
 		logger.Info().Msg("Starting RaftyMcRaftFace")
 	}
 
-	hclogger := raftyzerolog.HCLogger{Logger: subLogger}
-	raftylogger := raftyzerolog.RaftyLogger{Logger: subLogger.With().Str("name", "rafty").Logger()}
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 
+	// Rafty Disco
 	var err error
 	var discoverer interfaces.Discoverer
 	if optionNats {
 		logger.Info().Msg("Using NATS KV discovery")
-		if discoverer, err = makeNatsKVDiscoverer(ctx, &raftylogger); err != nil {
+		if discoverer, err = makeNatsKVDiscoverer(ctx, raftylogger); err != nil {
 			return err
 		}
 	} else if optionConsul {
 		logger.Info().Msg("Using Consul service discovery")
-		if discoverer, err = makeConsulServiceDiscoverer(ctx, &raftylogger, &hclogger); err != nil {
+		if discoverer, err = makeConsulServiceDiscoverer(ctx, raftylogger, consullogger); err != nil {
 			return err
 		}
 	} else {
 		logger.Info().Msg("Using local discovery")
-		if discoverer, err = makeLocalDiscoverer(ctx, &raftylogger); err != nil {
+		if discoverer, err = makeLocalDiscoverer(ctx, raftylogger); err != nil {
 			return err
 		}
 	}
 
-	works := &RaftyMcRaftFaceWorks[string]{
+	// Rafty Foreman
+	formeman := &RaftyMcRaftFaceWorks[string]{
 		works: []RaftyMcRaftFaceWork[string]{
 			"boating", "flying", "hiking", "running", "swimming",
 			"walking", "skiing", "sleeping", "eating", "drinking",
@@ -124,14 +109,13 @@ func run(cmd *cobra.Command, args []string) error {
 		ch: make(chan struct{}),
 	}
 
+	// Rafty
 	r, err := rafty.New[string, RaftyMcRaftFaceWork[string]](
-		discoverer,
-		works,
-		makeWork(&logger),
+		discoverer, formeman, makeWork(&logger),
 		rafty.RaftListeningAddressPort[string, RaftyMcRaftFaceWork[string]](optionBindAddress, optionPort),
 		rafty.RaftAdvertisedAddress[string, RaftyMcRaftFaceWork[string]](optionAdvertisedAddress),
-		rafty.Logger[string, RaftyMcRaftFaceWork[string]](&raftylogger),
-		rafty.HCLogger[string, RaftyMcRaftFaceWork[string]](&hclogger),
+		rafty.Logger[string, RaftyMcRaftFaceWork[string]](raftylogger),
+		rafty.HCLogger[string, RaftyMcRaftFaceWork[string]](raftlogger),
 	)
 	if err != nil {
 		cancel()
@@ -154,6 +138,47 @@ func run(cmd *cobra.Command, args []string) error {
 	<-r.Done()
 
 	return nil
+}
+
+func loggers() (logger zerolog.Logger, raftylogger interfaces.Logger, raftlogger hclog.Logger, consullogger hclog.Logger) {
+	zerolog.TimeFieldFormat = time.RFC3339Nano
+	consoleWriter := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: "Jan 2 15:04:05.000-0700"}
+	multi := zerolog.MultiLevelWriter(consoleWriter)
+	logger = zerolog.New(multi).With().Timestamp().Logger()
+
+	switch optionVerbose {
+	case 0:
+		logger = logger.With().Logger().Level(zerolog.InfoLevel)
+	case 1:
+		logger = logger.With().Logger().Level(zerolog.DebugLevel)
+	default:
+		logger = logger.With().Logger().Level(zerolog.TraceLevel)
+	}
+
+	subLogger := logger
+	if optionLogCaller {
+		logger = logger.With().Caller().Logger()
+		subLogger = subLogger.With().CallerWithSkipFrameCount(3).Logger()
+	}
+
+	// Rafty
+	raftylogger = &raftyzerolog.RaftyLogger{Logger: subLogger.With().Str("name", "rafty").Logger()}
+
+	// Raft
+	if optionLogRaft {
+		raftlogger = &raftyzerolog.HCLogger{Logger: subLogger}
+	} else {
+		raftlogger = hclog.NewNullLogger()
+	}
+
+	// Consul
+	if optionLogConsul {
+		consullogger = &raftyzerolog.HCLogger{Logger: subLogger}
+	} else {
+		consullogger = hclog.NewNullLogger()
+	}
+
+	return logger, raftylogger, raftlogger, consullogger
 }
 
 func makeNatsKVDiscoverer(ctx context.Context, logger interfaces.Logger) (interfaces.Discoverer, error) {
